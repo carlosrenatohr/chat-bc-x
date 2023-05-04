11376a56f8c75d59478d6cd7e00eae1951d72772
Carlos Hernandez <honchkrow1995@gmail.com>
2023-05-04 00:53:17 -0600

    feat: :technologist: Start basic webhook with BigCommerce connection
    I added the connection to BigCommerce and the response for Dialog Flow just like we had it before on Flask prototype app. A single request post to reply the call every time required.
    
    INIT

---
+++ bigCommerce.js
@@ -0,0 +1,47 @@
+// const BigCommerce = require('node-bigcommerce');
+// import fetch from 'node-fetch';
+const fetch = (...args) =>
+	import('node-fetch').then(({default: fetch}) => fetch(...args));
+
+const clientId = process.env.BIGCOMMERCE_CLIENT_ID;
+const accessToken = process.env.BIGCOMMERCE_ACCESS_TOKEN;
+const storeHash = process.env.BIGCOMMERCE_STORE_HASH;
+
+const base_url = `https://api.bigcommerce.com/stores/${storeHash}/`;
+// const bigcommerce = new BigCommerce({
+//     logLevel: 'info',
+//     clientId: process.env.BIGCOMMERCE_CLIENT_ID,
+//     accessToken: process.env.BIGCOMMERCE_ACCESS_TOKEN,
+//     storeHash: process.env.BIGCOMMERCE_STORE_HASH,
+//     // storeUrl: process.env.BIGCOMMERCE_STORE_URL,
+//     responseType: 'json',
+//     headers: { 'Accept-Encoding': '*' },
+//     // apiVersion: 'v3',
+// })
+
+const options = {
+    headers: {
+      'Accept': 'application/json',
+      'Accept-Encoding': '*',
+      'Content-Type': 'application/json',
+      'X-Auth-Token': accessToken,
+    }
+  };
+
+  const request = async (path, method = 'get') => {
+    const url = base_url + path;
+    // const url = "https://api.bigcommerce.com/stores/wp1youxqtj/v2/orders";
+    // options.method = method;
+    try {
+      const response = await fetch(url, options);
+      const data = await response.json();
+      console.log(data);
+      return data;
+    } catch (err) {
+        console.warn(err.status, err.statusText);
+        console.error(err);
+    }
+  }
+  
+
+module.exports = { request };
