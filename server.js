ce167832aaef77ac12ecc7e249ed44e14445d98f
Carlos Hernandez <honchkrow1995@gmail.com>
2023-06-19 17:55:28 -0600

    test: :boom: Add warning error test + expose server to work with tests

--- server.js
+++ server.js
@@ -1,4 +1,22 @@
-const app = require('./src/app');
-const PORT = process.env.PORT || 3001;
+const express = require("express");
+const app = express();
 
-const server = app.listen(PORT, () => console.log(`App listening on port ${PORT}!`));
+const middleware = require('./src/middlewares');
+const orderRouter = require('./src/routes/order');
+
+app.use(middleware);
+app.use(orderRouter);
+
+function startServer() {
+    const PORT = process.env.PORT || 3001;
+
+    const server = app.listen(PORT, () => {
+        console.log(`App listening on port ${PORT}!`)
+    });
+
+    return {app, server};
+}
+startServer();
+
+
+module.exports = startServer;
--- tests/server.test.js
+++ tests/server.test.js
@@ -1,5 +1,6 @@
-const supertest = require('supertest');
-const app = require('../src/app');
+const supertest = require("supertest");
+const startServer = require('../server');
+const { app, server } = startServer();
 const api = supertest(app);
 
 beforeEach(() => {
@@ -16,7 +17,7 @@ describe('GET /', () => {
 });
 
 describe('POST /webhook', () => {
-  it.skip('should return a valid response with order status and tracking link for a valid email', async () => {
+  it('should return a valid response with order status and tracking link for a valid email', async () => {
     const res = await api
         .post('/webhook')
         .send({
@@ -27,8 +28,20 @@ describe('POST /webhook', () => {
     expect(res.status).toEqual(200);
     expect(res.body.fulfillmentText).toContain('Great news!');
   });
+  
+  it('should return a warning response asking for more information', async () => {
+    const res = await api
+        .post('/webhook')
+        .send({
+          action: 'check_order_status',
+          parameters: { order_id: 6347, email: '' }
+      });
+
+    expect(res.status).toEqual(200);
+    expect(res.body.fulfillmentText).toContain(`I'm sorry, I don't understand. Could you provide more information?`);
+  });
 });
 
-afterAll(() => {
-    server.close();
+afterAll((done) => {
+    server.close(done);
 });
